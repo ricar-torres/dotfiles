d55830933de5643933249b0410b7a3b39fd126ee
Ricardo Torres <47981594+ricar-torres@users.noreply.github.com>
2024-08-19 14:43:15 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ 
+# Define your environment variable
+
+ENV_VARIABLE="export MassTransit__HostUri='sb://biobusdev-ricar-torres.servicebus.windows.net'"
+ 
+# Append the environment variable to .bashrc if it's not already present
+
+if ! grep -qF "$ENV_VARIABLE" ~/.bashrc; then
+
+    echo "$ENV_VARIABLE" >> ~/.bashrc
+
+fi
+ 
