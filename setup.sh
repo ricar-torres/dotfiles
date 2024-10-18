6b0f88bd81eeeb95941383cd66689357cf51fdfb
Ricardo Torres <47981594+ricar-torres@users.noreply.github.com>
2024-10-18 14:20:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define your environment variable
-ENV_VARIABLE="export MassTransit__HostUri='sb://biobusdev-00pb1.servicebus.windows.net'"
+ENV_VARIABLE="export MassTransit__HostUri='sb://biobusdev-00rtg1.servicebus.windows.net'"
 # Append the environment variable to .bashrc if it's not already present
 if ! grep -qF "$ENV_VARIABLE" ~/.bashrc; then
     echo "$ENV_VARIABLE" >> ~/.bashrc
